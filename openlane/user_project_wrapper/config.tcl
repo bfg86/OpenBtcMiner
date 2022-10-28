b4bd11555107ab3f3d960b52439e7e440ff3b502
bfg86 <bfg86@users.noreply.github.com>
2022-10-28 07:30:04 +0000

    reverting config.tcl to 0.4.0 version

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -53,14 +53,14 @@ set ::env(SYNTH_MUX4_MAP) ""
 set ::env(SYNTH_MUX_MAP) ""
 set ::env(SYNTH_BUFFERING) 1
 set ::env(SYNTH_SIZING) 1
-set ::env(SYNTH_STRATEGY) {AREA 3}
+set ::env(SYNTH_STRATEGY) {AREA 0}
 
 ## Floorplan
 set ::env(DESIGN_IS_CORE) 1
 
 ## Placement
 set ::env(PL_BASIC_PLACEMENT) 0
-set ::env(PL_TARGET_DENSITY) 0.54
+set ::env(PL_TARGET_DENSITY) 0.55
 set ::env(CELL_PAD) 4
 
 
